f0f3e35ef04231802d0a54140ad6766653526883
Bradley Sheehan <bradpsheehan@gmail.com>
2013-05-28 22:17:39 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtisticStreets::Application.config.secret_token = '2f1ebaaf759f9eeb7df18cc990a0a4d29331953b9992ac0d4004ff49ba116bd49df13dce8cc63d7f1533ee3649978950e115a451902650c286682fcc139e5db0'
