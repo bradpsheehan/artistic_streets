605cb97a9bd22abce59ffe30f75614d786266608
Bradley Sheehan <bradpsheehan@gmail.com>
2013-06-06 13:18:52 -0600

    localization working with dropdown on top nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,13 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # def get_locale
+  #   I18n.locale = params[:locale] || I18n.default_locale
+  # end
+  
   def get_locale
     I18n.locale = session[:i18n] || I18n.default_locale || :en
   end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ ArtisticStreets::Application.routes.draw do
   post '/arts/instagram', to: 'arts#instagram_art'
   get '/arts/instagram', to: 'arts#verify_instagram_art'
 
+  get '/i18n' => 'i18n#update'
+
   resources :arts
 end
