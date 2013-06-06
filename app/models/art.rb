a9484627c9fbf1f26288554f1893f5b7baa19b03
Bradley Sheehan <bradpsheehan@gmail.com>
2013-06-06 10:30:03 -0600

    seeds

--- app/models/art.rb
+++ app/models/art.rb
@@ -5,8 +5,18 @@ class Art < ActiveRecord::Base
   validates_associated :location
   accepts_nested_attributes_for :location
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, storage: :s3, styles: { medium: "300x300>", thumb: "100x100>" }
 
   validates :location, presence: true
+
+  def to_map_art
+    {
+      artist: artist,
+      title: title,
+      lat: location.latitude,
+      long: location.longitude,
+      image: image.url,
+    }
+  end
 
 end
