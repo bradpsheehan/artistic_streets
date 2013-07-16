c3b27494dd7af815ed54ffbe3c99ea1f7360ce7e
Laura Steadman <lauramsteadman@gmail.com>
2013-07-16 08:18:00 -0600

    Handlebars templating set up, need to fix carousel'

--- app/assets/javascripts/arts.js
+++ app/assets/javascripts/arts.js
@@ -7,24 +7,31 @@ $('#go-to-address').click(function() {
 
 var populateSideBar = function(art_id) {
   var url = '/arts/'+ art_id + '.json';
-  
   $.getJSON(url, function(data) {
-    var art = data.art
-    var imageTags = ""
+    var art = data.art;
+    var imageTags = "";
 
-    $.each(data.images, function(index, value){
-      return imageTags += '<li><img src="' + value + '"/></li>'
-    });
+    $.each(data.images, function(index, value){ return imageTags += '<li><img src="' + value + '"/></li>'; });
 
-    var addPhotoButton = "<form enctype='multipart/form-data' method='post' action='/images'><input type='file' name='image' id='add-photo-image'><input type='hidden' name='art_id' value='" + art.id +"id='add-photo-artid'><input type='submit' id='add-photo'></form>"
-    var sideBarHTML = '<h4><em>Title:</em> '+ art.title +'</h4><h5><em>Artist: </em>'+ art.artist+'</h5><p id="art-comment"><em>Comment:</em> '+ art.comment+'</p><div class="image_carousel"><ul id="carousel">' + imageTags + '</ul><div class="clearfix"></div><div id="carousel-buttons"><a class="prev custom-button" id="art-prev" href="#"><span>prev</span></a><a class="next custom-button" id="art-next" href="#"><span>next</span></a></div></div>' +addPhotoButton;
+    var source   = $("#sidebar-template").html();
+    var template = Handlebars.compile(source);
+    var context = { art: {id: art.id, title: art.title, artist: art.artist ,comment: art.comment }, imageTags: imageTags };
 
-    $('.side-nav').append(sideBarHTML);
-    $("#carousel").carouFredSel({
+    var html = template(context);
+    $('.side-nav').append(html);
+
+   $("#carousel").carouFredSel(
+      {
         auto : false,
-        items: { minimum: 1, visible: "variable", start: "random", width: 200, height: 200 },
+        items: { 
+                  minimum: 1,
+                  visible: 'variable,
+                  start: "random",
+                  width: 200,
+                  height: 200
+                  },
         prev : "#art-prev",
         next : "#art-next"
-    });
+      });
   });
 }
